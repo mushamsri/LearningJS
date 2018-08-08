cfa390a84364c55fd47b30de70ae21108f0d2817
Musham <aruna.sri.musham@accenture.com>
2018-08-08 12:55:40 +0530

    object basics

---
+++ objects/objectaccessing.js
@@ -0,0 +1,8 @@
+let person= {
+    name:'Aruna',
+    age:22,
+    location:'adilabad'
+}
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+person.age=person.age+1
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)

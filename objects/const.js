4153a01f78f08a45d5a383981b47c71e33ad9c05
Musham <aruna.sri.musham@accenture.com>
2018-08-14 13:03:47 +0530

    variables with var

---
+++ objects/const.js
@@ -0,0 +1,15 @@
+/*let isit=true
+isit=false
+let isit=hi
+console.log(isit)*/
+
+/*var isit=true
+isit=false
+var isit='hi'
+console.log(isit)*/
+
+console.log(age)
+var age=10  //result is undefined
+
+console.log(age)
+let age=10  //error
